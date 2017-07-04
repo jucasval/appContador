bc793336c471ba34d25c9b6f248c3daf6df16ea5
jucasval <juanfrancastilla@gmail.com>
2017-07-04 12:47:12 +0200

    Prueba contador

---
+++ app/src/main/java/com/jucasval/jucasval/contador/MainActivity.java
@@ -0,0 +1,48 @@
+package com.jucasval.jucasval.contador;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    public int contador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contador = 0;
+    }
+
+    public void incrementar(View vista){
+
+        contador++;
+        mostrarResultado();
+
+    }
+
+    public void decrementar(View vista){
+
+        contador--;
+        mostrarResultado();
+
+    }
+
+    public void resetear(View vista){
+
+        contador = 0;
+        mostrarResultado();
+
+    }
+
+    public void mostrarResultado(){
+
+        TextView textoResultado = (TextView) findViewById(R.id.textContador);
+
+        textoResultado.setText("Contador: "+contador);
+
+    }
+}//fin class MainActivity
